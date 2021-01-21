0bcf7bb578c5222ecc75a057286d2e668da21bcb
durgesh-j24 <77041335+durgesh-j24@users.noreply.github.com>
2021-01-21 11:06:54 +0530

    lops
    for loop

---
+++ y.java
@@ -0,0 +1,6 @@
+Java for Loop
+Java for loop is used to run a block of code for a certain number of times. The syntax of for loop is:
+
+for (initialExpression; testExpression; updateExpression) {
+    // body of the loop
+}
